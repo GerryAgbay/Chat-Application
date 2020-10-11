a3db5c45fb71705bbf85f465e50d8997b561620e
GerryAgbay <gerryagbayjr@gmail.com>
2020-10-11 23:53:47 +0000

    Transfer files from lect11-starter to serve as a base for the chat app

---
+++ scripts/Button.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Socket } from './Socket';
+
+function handleSubmit(event) {
+    let newMessage = document.getElementById("address_input");
+    Socket.emit('new address input', {
+        'address': newMessage.value,
+    });
+    
+    console.log('Sent the address ' + newMessage.value + ' to server!');
+    newMessage.value = ''
+    
+    event.preventDefault();
+}
+
+export function Button() {
+    return (
+        <form onSubmit={handleSubmit}>
+            <input id="address_input" placeholder="Enter a USPS address"></input>
+            <button>Add to DB!</button>
+        </form>
+    );
+}
