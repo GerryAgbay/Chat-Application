73759b0b7b38b9fa51047734399961b68efadcae
GerryAgbay <gerryagbayjr@gmail.com>
2020-10-13 12:02:29 +0000

    Add character theme for chat bot. Change background colors of chat app.

--- app.py
+++ app.py
@@ -90,22 +90,24 @@ def botDB(botName, botMsg):
 def bot(data):
     inputString = data["message"]
     inputList = inputString.split(" ")
-    botName = "Bot"
+    botName = "Halfbot"
     
     if (inputList[0] == "!!"):
         if (len(inputList) == 1):
-            botMsg = "Sorry, I don't recognize that command. Enter '!! help' to get a list of my commands."
+            botMsg = "I'm smart but I don't know everything. Enter '!! help' and try again with something I actually understand."
             botDB(botName, botMsg)
             
-        if (inputList[1] == "about") or (inputList[1] == "ABOUT"):
-            botMsg = "My name is Bot. I am a chat bot that has different functionalities. Enter '!! help' to get a list of my commands."
+        if (inputList[1] == "about") or (inputList[1] == "ABOUT") or (inputList[1] == "About"):
+            botMsg = ("I'm the Halfbot. I may be stuck here but you know what? If you're going to be a cripple, it's better to be a rich cripple. " + 
+            "A mind needs books and everything's better with some wine in the belly. That's what I do: I drink and I know things. " + 
+            "If you want to know what you can ask from me, enter '!! help'. I can't make any promises though.")
             botDB(botName, botMsg)
         
-        elif (inputList[1] == "help") or (inputList[1] == "HELP"):
-            botMsg = "Here is a list of my commands: [!! about, !! help, !! funtranslate <message>, !! randint <min> <max>]"
+        elif (inputList[1] == "help") or (inputList[1] == "HELP") or (inputList[1] == "Help"):
+            botMsg = "Here's a list of what I know: [!! about, !! help, !! funtranslate <message>, !! randjoke, !! randint <min> <max>]"
             botDB(botName, botMsg)
             
-        elif (inputList[1] == "funtranslate") or (inputList[1] == "FUNTRANSLATE"):
+        elif (inputList[1] == "funtranslate") or (inputList[1] == "FUNTRANSLATE") or (inputList[1] == "Funtranslate"):
             translate_url = "https://api.funtranslations.com/translate/dothraki.json?text=" + inputString[16:]
             translate_response = requests.request("GET", translate_url)
             translate_dictionary = translate_response.json()
@@ -113,28 +115,27 @@ def bot(data):
             if ("contents" in translate_dictionary.keys()):
                 translate_contents = translate_dictionary["contents"]
                 translated = translate_contents["translated"]
-                botMsg = "Dothraki Translation: " + translated
+                botMsg = "In Dothraki: " + translated
                 botDB(botName, botMsg)
                 
             elif ("error" in translate_dictionary.keys()):
                 error_msg = translate_dictionary["error"]["message"]
-                botMsg = error_msg
+                botMsg = "This is what my sources say: " + error_msg
                 botDB(botName, botMsg)
                 
-        elif (inputList[1] == "randint") or (inputList[1] == "RANDINT") and isinstance(int(inputList[2]), int) and isinstance(int(inputList[3]), int):
+        elif (inputList[1] == "randint") or (inputList[1] == "RANDINT") or (inputList[1] == "Randint") and isinstance(int(inputList[2]), int) and isinstance(int(inputList[3]), int):
             integer = random.randint(int(inputList[2]), int(inputList[3]))
-            botMsg = "Your random integer is: " + str(integer)
+            botMsg = "Here's a random integer: " + str(integer)
             botDB(botName, botMsg)
             
-        elif (inputList[1] == "randjoke") or (inputList[1] == "RANDJOKE"):
+        elif (inputList[1] == "randjoke") or (inputList[1] == "RANDJOKE") or (inputList[1] == "Randjoke"):
             joke_url = "https://sv443.net/jokeapi/v2/joke/Any?"
             joke_response = requests.request("GET", joke_url)
             joke_dictionary = joke_response.json()
-            print(joke_dictionary)
             
             if ("joke" in joke_dictionary.keys()):
                 joke_contents = joke_dictionary["joke"]
-                botMsg = "Joke: " + joke_contents
+                botMsg = joke_contents
                 botDB(botName, botMsg)
                 
             elif ("setup" in joke_dictionary.keys()):
@@ -143,7 +144,7 @@ def bot(data):
                 botDB(botName, botMsg)
             
         else:
-            botMsg = "Sorry, I don't recognize that command. Enter '!! help' to get a list of my commands."
+            botMsg = "I'm smart but I don't know everything. Enter '!! help' and try again with something I actually understand."
             botDB(botName, botMsg)
 
 
