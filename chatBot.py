15420c8a69a2d742fee627e1bd8e808954c2ca69
GerryAgbay <gerryagbayjr@gmail.com>
2020-10-26 05:02:20 +0000

    Cover 100% testing of chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -4,79 +4,62 @@ import requests
 import random
 from flask import request
 
-
-# def botDB(botName, botMsg):
-#     app.db.session.add(models.Chat(botName.upper() + ": " + botMsg.upper(), request.sid));
-#     app.db.session.commit();
-    
     
 def bot(data):
     inputString = data["message"]
     inputList = inputString.split(" ")
-    botName = "Halfbot"
     
     if (inputList[0] == "!!"):
         if (len(inputList) == 1):
             botMsg = "I'm smart but I don't know everything. Enter '!! help' and try again with something I actually understand."
-            #botDB(botName, botMsg)
             return botMsg
             
         if (inputList[1] == "about") or (inputList[1] == "ABOUT") or (inputList[1] == "About"):
             botMsg = ("I'm the Halfbot. I may be stuck here but you know what? If you're going to be a cripple, it's better to be a rich cripple. " + 
             "A mind needs books and everything's better with some wine in the belly. That's what I do: I drink and I know things. " + 
             "If you want to know what you can ask from me, enter '!! help'. I can't make any promises though.")
-            #botDB(botName, botMsg)
             return botMsg
         
         elif (inputList[1] == "help") or (inputList[1] == "HELP") or (inputList[1] == "Help"):
             botMsg = "Here's a list of what I know: [!! about, !! help, !! funtranslate <message>, !! randjoke, !! randint <min> <max>]"
-            #botDB(botName, botMsg)
             return botMsg
             
         elif (inputList[1] == "funtranslate") or (inputList[1] == "FUNTRANSLATE") or (inputList[1] == "Funtranslate"):
             translate_url = "https://api.funtranslations.com/translate/dothraki.json?text=" + inputString[16:]
             translate_response = requests.get(translate_url)
-            #print (translate_response)
             translate_dictionary = translate_response.json()
-            #print(translate_dictionary)
                 
             if ("contents" in translate_dictionary.keys()):
                 translate_contents = translate_dictionary["contents"]
                 translated = translate_contents["translated"]
                 botMsg = "In Dothraki: " + translated
-                #botDB(botName, botMsg)
                 return botMsg
                 
             elif ("error" in translate_dictionary.keys()):
                 error_msg = translate_dictionary["error"]["message"]
                 botMsg = "This is what my sources say: " + error_msg
-                #botDB(botName, botMsg)
                 return botMsg
                 
         elif (inputList[1] == "randint") or (inputList[1] == "RANDINT") or (inputList[1] == "Randint") and isinstance(int(inputList[2]), int) and isinstance(int(inputList[3]), int):
             integer = random.randint(int(inputList[2]), int(inputList[3]))
             botMsg = "Here's a random integer: " + str(integer)
-            #botDB(botName, botMsg)
             return botMsg
             
         elif (inputList[1] == "randjoke") or (inputList[1] == "RANDJOKE") or (inputList[1] == "Randjoke"):
             joke_url = "https://sv443.net/jokeapi/v2/joke/Any?"
-            joke_response = requests.request("GET", joke_url)
+            joke_response = requests.get(joke_url)
             joke_dictionary = joke_response.json()
             
             if ("joke" in joke_dictionary.keys()):
                 joke_contents = joke_dictionary["joke"]
                 botMsg = joke_contents
-                #botDB(botName, botMsg)
                 return botMsg
                 
             elif ("setup" in joke_dictionary.keys()):
                 joke_setup = joke_dictionary["setup"] + " " + joke_dictionary["delivery"]
                 botMsg = joke_setup
-                #botDB(botName, botMsg)
                 return botMsg
             
         else:
             botMsg = "I'm smart but I don't know everything. Enter '!! help' and try again with something I actually understand."
-            #botDB(botName, botMsg)
             return botMsg
--- mocked_unit_tests.py
+++ mocked_unit_tests.py
@@ -46,8 +46,10 @@ class MockedGetResponse:
             
     def json(self):
         return self.json_data
+        
+#-------
             
-class BotTestFuntranslate(unittest.TestCase):
+class BotTestFuntranslate1(unittest.TestCase):
     def setUp(self):
         self.success_test_params = [
             {
@@ -68,7 +70,77 @@ class BotTestFuntranslate(unittest.TestCase):
         expected = test1[KEY_EXPECTED]
         self.assertEqual(response, expected[KEY_BOT_MSG])
         
+#-------
+            
+class BotTestFuntranslate2(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: {"message" : "!! funtranslate reached limit"},
+                KEY_EXPECTED : {
+                    KEY_BOT_MSG : "This is what my sources say: Too Many Requests: Rate limit of 5 requests per hour exceeded. Please wait for 6 minutes and 24 seconds."
+                }
+            }
+        ]
         
+    def mocked_request(self, url):
+        return MockedGetResponse({'error': {'code': 429, 'message': 'Too Many Requests: Rate limit of 5 requests per hour exceeded. Please wait for 6 minutes and 24 seconds.'}}, 429)
+        
+    def testBotFuntranslateSuccess(self):
+        test1 = self.success_test_params[0]
+        with mock.patch('requests.get', self.mocked_request):
+            response = bot(test1[KEY_INPUT])
+        expected = test1[KEY_EXPECTED]
+        self.assertEqual(response, expected[KEY_BOT_MSG])
+        
+#-------
+            
+class BotTestRandjoke1(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: {"message" : "!! randjoke"},
+                KEY_EXPECTED : {
+                    KEY_BOT_MSG : "A web developer walks into a restaurant. He immediately leaves in disgust as the restaurant was laid out in tables."
+                }
+            }
+        ]
+        
+    def mocked_request(self, url):
+        return MockedGetResponse({'error': False, 'category': 'Programming', 'type': 'twopart', 'setup': 'A web developer walks into a restaurant.', 
+        'delivery': 'He immediately leaves in disgust as the restaurant was laid out in tables.', 'flags': {'nsfw': False, 'religious': False, 
+        'political': False, 'racist': False, 'sexist': False}, 'id': 6, 'lang': 'en'}, 200)
+        
+    def testBotRandjokeSuccess(self):
+        test1 = self.success_test_params[0]
+        with mock.patch('requests.get', self.mocked_request):
+            response = bot(test1[KEY_INPUT])
+        expected = test1[KEY_EXPECTED]
+        self.assertEqual(response, expected[KEY_BOT_MSG])
+        
+#------
+
+class BotTestRandjoke2(unittest.TestCase):
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: {"message" : "!! randjoke"},
+                KEY_EXPECTED : {
+                    KEY_BOT_MSG : "I've got a really good UDP joke to tell you but I don’t know if you'll get it."
+                }
+            }
+        ]
+        
+    def mocked_request(self, url):
+        return MockedGetResponse({'error': False, 'category': 'Programming', 'type': 'single', 'joke': "I've got a really good UDP joke to tell you but I don’t know if you'll get it.", 'flags': {'nsfw': False, 'religious': False, 'political': False, 'racist': False, 'sexist': False}, 'id': 0, 'lang': 'en'}, 200)
+        
+    def testBotRandjokeSuccess(self):
+        test1 = self.success_test_params[0]
+        with mock.patch('requests.get', self.mocked_request):
+            response = bot(test1[KEY_INPUT])
+        expected = test1[KEY_EXPECTED]
+        self.assertEqual(response, expected[KEY_BOT_MSG])
+
             
 if __name__ == '__main__':
     unittest.main()
