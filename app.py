16f49a615bec3333a0cde717589f441750891746
GerryAgbay <gerryagbayjr@gmail.com>
2020-10-27 05:10:51 +0000

    change IP and Port for CircleCI

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
     init_db(app)
     socketio.run(
         app,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=int(os.getenv('PORT', 8080)),
+        host=os.getenv('IP', '127.0.0.1'),
+        port=int(os.getenv('PORT', 5432)),
         debug=True
     )
