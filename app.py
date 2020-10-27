49f7412afebdd3c1374cc7e5ff623c80cae5ff9d
GerryAgbay <gerryagbayjr@gmail.com>
2020-10-27 18:38:56 +0000

    Organize app.py using black. Update package.json

--- app.py
+++ app.py
@@ -7,41 +7,44 @@ import flask_socketio
 from flask import request
 import re
 
-MESSAGES_RECEIVED_CHANNEL = 'messages received'
+MESSAGES_RECEIVED_CHANNEL = "messages received"
 
 app = flask.Flask(__name__)
 
 socketio = flask_socketio.SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
-dotenv_path = join(dirname(__file__), 'sql.env')
+dotenv_path = join(dirname(__file__), "sql.env")
 load_dotenv(dotenv_path)
 
-database_uri = os.environ['DATABASE_URL'] 
+database_uri = os.environ["DATABASE_URL"]
 
-app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
 
 db = flask_sqlalchemy.SQLAlchemy(app)
+
+
 def init_db(app):
     db.init_app(app)
     db.app = app
     db.create_all()
     db.session.commit()
 
+
 import models
 
 
 def emit_all_messages(channel):
-    all_messages = [ \
-        db_message.chat_message for db_message in \
-        db.session.query(models.Chat).all()]
-        
-    socketio.emit(channel, { 'allMessages': all_messages })
-    
-    
+    all_messages = [
+        db_message.chat_message for db_message in db.session.query(models.Chat).all()
+    ]
+
+    socketio.emit(channel, {"allMessages": all_messages})
+
+
 def push_new_user_to_db(name, auth_type, email, sid):
-        db.session.add(models.AuthUser(name, auth_type, email, sid));
-        db.session.commit();
+    db.session.add(models.AuthUser(name, auth_type, email, sid))
+    db.session.commit()
 
 
 def findUrl(data, sid):
@@ -50,81 +53,92 @@ def findUrl(data, sid):
     r_url = re.compile(r"^https?:")
     r_image = re.compile(r".*\.(jpg|png|gif)$")
     if r_url.match(item) or r_image.match(item):
-            db.session.add(models.Chat(item, user_sid));
-            db.session.commit();
-                
+        db.session.add(models.Chat(item, user_sid))
+        db.session.commit()
 
-@socketio.on('connect')
+
+@socketio.on("connect")
 def on_connect():
-    print('Someone connected!')
-    
-    socketio.emit('status', {'count': count})
-    socketio.emit('connected', { 'test': 'Connected'})
-    
-    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
-    
-    
-count = 0    
+    print("Someone connected!")
 
-@socketio.on('new google user')
+    socketio.emit("status", {"count": count})
+    socketio.emit("connected", {"test": "Connected"})
+
+    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
+
+
+count = 0
+
+
+@socketio.on("new google user")
 def on_new_google_user(data):
     print("Got an event for new google user input with data:", data)
-    
+
     global count
     count += 1
-    
-    socketio.emit('status', {'count': count})
 
-@socketio.on('new google user 2')
+    socketio.emit("status", {"count": count})
+
+
+@socketio.on("new google user 2")
 def on_new_google_user_2(data):
-    push_new_user_to_db(data['name'], models.AuthUserType.GOOGLE, data['email'], request.sid)
-    
-    
-@socketio.on('disconnect')
+    push_new_user_to_db(
+        data["name"], models.AuthUserType.GOOGLE, data["email"], request.sid
+    )
+
+
+@socketio.on("disconnect")
 def on_disconnect():
-    print ('Someone disconnected!')
-    
+    print("Someone disconnected!")
+
     global count
     if count > 0:
         count -= 1
     else:
         count = 0
-        
-    socketio.emit('status', {'count': count})
-    
 
-@socketio.on('new message input')
+    socketio.emit("status", {"count": count})
+
+
+@socketio.on("new message input")
 def on_new_message(data):
     print("Got an event for new message input with data:", data)
-    user = [ \
-        db_user.name for db_user in \
-        db.session.query(models.AuthUser).filter_by(sid=request.sid)]
-    
-    db.session.add(models.Chat(user[len(user)-1] + ": " + data["message"], request.sid));
-    db.session.commit();
-    
+    user = [
+        db_user.name
+        for db_user in db.session.query(models.AuthUser).filter_by(sid=request.sid)
+    ]
+
+    db.session.add(
+        models.Chat(user[len(user) - 1] + ": " + data["message"], request.sid)
+    )
+    db.session.commit()
+
     findUrl(data["message"], request.sid)
-    
+
     from chatBot import bot
+
     if bot(data):
         botName = "Halfbot"
-        db.session.add(models.Chat(botName.upper() + ": " + bot(data).upper(), request.sid));
-        db.session.commit();
-    
+        db.session.add(
+            models.Chat(botName.upper() + ": " + bot(data).upper(), request.sid)
+        )
+        db.session.commit()
+
     emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
     return flask.render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     init_db(app)
     socketio.run(
         app,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=int(os.getenv('PORT', 8080)),
-        debug=True
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", 8080)),
+        debug=True,
     )
